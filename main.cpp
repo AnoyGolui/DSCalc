5828f8a6532e4a0e72442947f7962340a5d3598a
AnoyGolui <67983302+AnoyGolui@users.noreply.github.com>
2021-08-02 00:24:34 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 //quicksort
 void quickSort(int number[25],int first,int last)
-{int i, j, pivot, temp;
-
+{
+   int i, j, pivot, temp;
    if(first<last){
       pivot=first;
       i=first;
